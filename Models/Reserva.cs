bb1b8347f0cab49ad2740c3182a2bc1b7503ad06
Bloark <wildson.luiz@gmail.com>
2022-09-19 17:02:25 -0300

    Implementado Calculo Diária

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -47,8 +47,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            // *IMPLEMENTE AQUI* OK
+            decimal valor = DiasReservados*Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
