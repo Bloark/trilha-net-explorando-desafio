96a307d17f7dba4f3d507c0cd41f6e49410704e7
Bloark <wildson.luiz@gmail.com>
2022-09-19 16:57:43 -0300

    Implementado if numero de hospedes e Exceção para capacidade.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,18 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // *IMPLEMENTE AQUI* OK
+            
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
-            {
+            {                   
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTE AQUI* OK
+                throw new Exception("Numero de Hospedes maior que a capacidade");
+               
             }
         }
 
